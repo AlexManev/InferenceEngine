d4b188afb72d8da052c272601e55d8cda067ef62
Aleksandar Manev <amanev95@gmail.com>
2017-05-17 01:12:47 +1000

    Rebuilt the BC Algorithm

--- src/BackwardsChaining.java
+++ src/BackwardsChaining.java
@@ -1,57 +1,120 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-public class BackwardsChaining extends Method{
+/**
+ * Backwards Chaining, Works backwards for the query, if the query is known to
+ * be true then no work is needed. Otherwise, the algorithm finds those
+ * implications in the knowledge base whose conclusion is the query. If all the
+ * premises of one of those implications can be proven true (by backwards
+ * caning), then query is true.
+ * 
+ * @author Aleksandar_Manev
+ */
+public class BackwardsChaining extends Method {
 	private String _query;
-	private List<String> _trueValues;
-	private List<String> _frontier;
+	private List<String> _knownRules;
+
 	public BackwardsChaining(List<String> kb, String query) {
 		super(kb);
-		_trueValues = new ArrayList<String>();
-		_frontier = new ArrayList<String>();
+		_knownRules = new ArrayList<String>();
 		_query = query;
-		_frontier.add(_query);
+		InitTrueVals();
 	}
-	
+
+	/**
+	 * This is where the main logic for backwards chaining sits, calling this
+	 * function will start the backwards chaining algorithm and print the
+	 * result.
+	 * 
+	 * @param NLT natural language translator, if you don't have one pass in null
+	 */
 	@Override
 	public void solve(NLTranslator NLT) {
 		boolean result = false;
-		while (!_frontier.isEmpty()){
-			String current = _frontier.get(0);
-			for(String rule : _kb) {
-				String[] parts = rule.split("=>");
-				if(parts.length == 2){
-					if(current.equals(parts[1])) {
-						for(String var : parts[0].split("&")) {
-							if(!_frontier.contains(var) && !_trueValues.contains(var)){
-								_frontier.add(var);
-							}
-						}
-					}
-				}
-			}
-			_trueValues.add(current);
-			_frontier.remove(current);
-		}
-		if(_trueValues.size() > 1)
+		//if the query is known to be true
+		if (_trueValues.contains(_query)) {
+			System.out.println(_query + " is True Fact");
+			_knownRules.add(_query);
 			result = true;
-		
-		End(result,NLT);
+		} else {
+			//otherwise perform DFS to discover all unknown variables related to the query.
+			result = Search(_query);
+		}
+		PrintResult(result, NLT);
 	}
 
-	private void End(boolean result, NLTranslator NLT) {
+	/**
+	 * Given the known knowledge base rules this function performs
+	 * Depth-first-search backwards to find the value of the variable.
+	 * 
+	 * @param current variable that needs to be explored
+	 * @return value for the variable
+	 */
+	private boolean Search(String current) {
+		boolean result = false;
+		// for every rule in the knowledge base
+		NextRule:
+		for (String rule : _kb) {
+			String[] parts = rule.split("=>");
+			if(parts.length == 2){
+				if (current.equals(parts[1])) {
+					//repetitive checking
+					if (_knownRules.contains(rule))
+						continue;
+					else
+						_knownRules.add(rule);
+					//for each variable on the left side of the implication sign
+					for (String var : parts[0].split("&")) {
+						if (!_trueValues.contains(var)) {
+							//if at least one var on the left side is false then
+							//this rule is false try another rule.
+							if (!Search(var)) {
+								result = false;
+								continue NextRule;
+							}
+							//add var to known true variables
+							_trueValues.add(var);
+						} else {
+							_knownRules.add(var);
+						}
+					}
+					//if all variables pass as true then return true.
+					result = true;
+				}
+			}
+		}
+		//Can't find any matching rule for this var.
+		return result;
+	}
+	/**
+	 * Print the result of the search.
+	 * YES: followed by all variables included in the search - if the query is proven true.
+	 * NO: - if the query can not be proven.
+	 * @param result value of the query variable.
+	 * @param NLT natural language translator
+	 */
+	private void PrintResult(boolean result, NLTranslator NLT) {
 		if (result) {
 			System.out.print("YES:");
-			Collections.reverse(_trueValues);
-			for (String element : _trueValues) {
+			Set<String> visitedSet = new HashSet<>();
+			for (String rule : _knownRules) {
+				String[] parts = rule.split("=>");
+				if (parts.length == 1) {
+					visitedSet.add(parts[0]);
+				} else {
+					visitedSet.add(parts[1]);
+				}
+			}
+			for (String element : visitedSet) {
 				if (NLT == null)
 					System.out.print(" " + element);
 				else
-					System.out.print(" " + NLT.GetNLfromVar(element)+".");
+					System.out.print(" " + NLT.GetNLfromVar(element) + ".");
 			}
 		} else
 			System.out.println("NO:");
-		
+
 	}
 }
