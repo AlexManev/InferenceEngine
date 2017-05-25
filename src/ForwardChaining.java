d11255e40ddab7ed325bda482904762b328c776c
Aleksandar Manev <amanev95@gmail.com>
2017-05-25 13:32:31 +1000

    fixed known-fact bug and added comments on ForwardChaining

--- src/ForwardChaining.java
+++ src/ForwardChaining.java
@@ -1,24 +1,27 @@
 import java.util.*;
-
+/**
+ * Forward Chaining,  The idea is to find all implications that have p as a premise; then if the 
+ * other premises are already known to hold, we can add the consequent of the implication to the 
+ * knowledge base, triggering further inference.
+ * 
+ * @author Cameron_Watts
+ */
 public class ForwardChaining extends Method{
 	private String _query;
-	private List<String> _trueValues;
 	public ForwardChaining(List<String> kb, String query) {
 		super(kb);
-		_trueValues = new ArrayList<String>();
 		_query = query;
 		InitTrueVals();
 	}
 	
-	private void InitTrueVals() {
-		for(String rule : _kb) {
-			if(rule.split("=>").length == 1) {
-				_trueValues.add(rule);
-			}
-		}
-	}
-
-	private void End(boolean result, NLTranslator NLT) {
+	/**
+	 * Print the result of the search.
+	 * YES: followed by all variables included in the search - if the query is proven true.
+	 * NO: - if the query can not be proven.
+	 * @param result value of the query variable.
+	 * @param NLT natural language translator
+	 */
+	private void PrintResult(boolean result, NLTranslator NLT) {
 		if (result) {
 			System.out.print("YES:");
 			for (String element : _trueValues) {
@@ -30,28 +33,45 @@ public class ForwardChaining extends Method{
 		} else
 			System.out.println("NO:");
 	}
-
+	/**
+	 * This is where the main logic for forward chaining sits, calling this
+	 * function will start the forward chaining algorithm and print the
+	 * result.
+	 * 
+	 * @param NLT natural language translator, if you don't have one pass in null
+	 */
 	@Override
 	public void solve(NLTranslator NLT) {
 		boolean result = false;
 		boolean validRule = true;
+		//is the query is known to be true
+		for(String true_var : _trueValues)
+			if(true_var.equals(_query))
+				result = true;
+		//loop until you find a solution
 		while (!result) {
 			boolean newAdded = false;
+			//for every rule in the KB check if there are new rules proven true.
 			for(String rule : _kb) {
 				String[] parts = rule.split("=>");
+				//its a rule
 				if(parts.length == 2){
 					validRule = true;
+					//get the variables on the left side of the implementation.
 					for(String depVar : parts[0].split("&")) {
+						//if at least 1 variable is unknown then this rule can't be proven so go to the next rule.
 						if(!_trueValues.contains(depVar)) {
 							validRule = false;
 							break;
 						}
 					}
+					//if rule is proven true add, add the newly proven variable to the _trueVariables list.
 					if(validRule) {
 						if(!_trueValues.contains(parts[1])) {
 							newAdded = true;
 							_trueValues.add(parts[1]);
 						}
+						//goal check - check if the newly proven variable is the same as the query.
 						if(parts[1].equals(_query)) {
 							result = true;
 							break;
@@ -59,11 +79,10 @@ public class ForwardChaining extends Method{
 					}
 				}
 			}
+			//if you can't prove any more rules break the search loop. 
 			if(!newAdded)
 				break;
 		}
-		End(result,NLT);
+		PrintResult(result,NLT);
 	}
-	
-	
 }
